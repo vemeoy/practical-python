d98fa38c600799ffd3ec877c65e088ca8b89af43
eo-y <eoy@localhost.localdomain>
2020-11-22 20:51:11 +0100

    training'e ilk commit

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height=100.0 #meters
+count=1
+
+while count < 11:
+    height = height * 0.6    
+    print(count, round(height,4))
+    count = count + 1
+
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,33 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month=0
+extra_start_month=60
+extra_end_month=108
+extra_payment=1000
+
+
+while principal > 0:
+    month = month + 1
+
+    if month >= extra_start_month and month < extra_end_month:
+        extra = extra_payment
+    else:
+        extra = 0
+
+    if principal < (payment + extra):
+        total_paid = total_paid + principal
+        principal = 0
+    else:
+        principal = principal * (1+rate/12) - (payment + extra)
+        total_paid = total_paid + payment + extra
+
+    print(month, round(total_paid,2), round(principal,2))
+
+print('Total paid', round(total_paid, 2))
+print('Months', month)
